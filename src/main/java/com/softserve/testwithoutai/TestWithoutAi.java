528f1e38066d1e6c96c6e18aa3c64d5d3c5a2666
Marharyta Kozub <MARHARYTAKO@ext.inditex.com>
2023-09-08 11:45:18 +0300

    UC#7 Infrastructure setup

---
+++ src/main/java/com/softserve/testwithoutai/TestWithoutAi.java
@@ -0,0 +1,9 @@
+package com.softserve.testwithoutai;
+
+public class TestWithoutAi {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/softserve/testwithoutai/TestWithoutAiTest.java
@@ -0,0 +1,5 @@
+package com.softserve.testwithoutai;
+
+class TestWithoutAiTest {
+
+}
